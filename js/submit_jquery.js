6b9ce9e59a21dc5824fff9efca9245ae80e4d9ab
pramodhs <pramod.shashidhara@gmail.com>
2018-09-23 23:37:13 -0700

    Life to contact Us

--- js/submit_jquery.js
+++ js/submit_jquery.js
@@ -1,28 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Submit form with id function.
-    $("#btn_id").click(function() {
+    $("#btn_id").click(function () {
         var name = $("#txtName").val();
         var email = $("#txtEmail").val();
         var subject = $("#txtsubject").val();
         var message = $("#txtmessage").val();
         if (validation()) // Calling validation function.
         {
-            BootstrapDialog.show({
-                type: BootstrapDialog.TYPE_WARNING,
-                title: 'HungryHour',
-                message: 'Thank you for contacting us! We will get in touch with you as soon as possible.',
-                buttons: [{
-                    label: 'Close',
-                 action: function(dialog) {
-                     dialog.close();
-                 }
-                }],
-                cssClass: 'bootstrap-dialog'
+            var contactUS = {
+                name: name,
+                email: email,
+                subject: subject,
+                comments: message
+            }
+
+            $('#target').html('sending..');
+
+            $.ajax({
+                url: 'https://www.itshungryhour.com/api/v1/contactus',
+                type: 'post',
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (data) {
+                    BootstrapDialog.show({
+                        type: BootstrapDialog.TYPE_WARNING,
+                        title: 'HungryHour',
+                        message: 'Thank you for contacting us! We will get in touch with you as soon as possible.',
+                        buttons: [{
+                            label: 'Close',
+                            action: function (dialog) {
+                                dialog.close();
+                            }
+                        }],
+                        cssClass: 'bootstrap-dialog'
+                    });
+                    $("#txtName").val("");
+                    $("#txtEmail").val("");
+                    $("#txtsubject").val("");
+                    $("#txtmessage").val("");
+                },
+                error: function (request, status, error) {
+                    BootstrapDialog.show({
+                        type: BootstrapDialog.TYPE_WARNING,
+                        title: 'Error',
+                        message: 'Sorry for the inconvience, you cannot reach hungry hour at this moment. Please try again later.',
+                        buttons: [{
+                            label: 'Close',
+                            action: function (dialog) {
+                                dialog.close();
+                            }
+                        }],
+                    });
+                },
+                data: JSON.stringify(contactUS)
             });
-            $("#txtName").val("");
-            $("#txtEmail").val("");
-            $("#txtsubject").val("");
-            $("#txtmessage").val("");
         }
     });
 
@@ -39,9 +70,9 @@ $(document).ready(function() {
                 message: 'Please fill name and message!',
                 buttons: [{
                     label: 'Close',
-                 action: function(dialog) {
-                     dialog.close();
-                 }
+                    action: function (dialog) {
+                        dialog.close();
+                    }
                 }],
             });
             return false;
@@ -52,9 +83,9 @@ $(document).ready(function() {
                 message: 'Invalid Email!',
                 buttons: [{
                     label: 'Close',
-                 action: function(dialog) {
-                     dialog.close();
-                 }
+                    action: function (dialog) {
+                        dialog.close();
+                    }
                 }],
             });
             return false;
